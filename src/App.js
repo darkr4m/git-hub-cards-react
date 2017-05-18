5d003c889eee47ea1d99fae02725576ff66bdb80
darkr4m <blackr4m@gmail.com>
2017-05-17 21:33:05 -0400

    Insert styles from markup. Build basic card function component. Remove default React Markup.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Card = (props) => {
+    return (
+        <div className="cardx row">
+            <div className="col-3">
+                <img className="gravicon" src="http://placehold.it/65" />
+            </div>
+            <div className="col-9 heading">
+                <h4 className="card-name">Maxi Wright</h4>
+                <p>darkr4m</p>
+                <p>Athens, GA</p>
+            </div>
+        </div>
+    )
+};
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Card />
       </div>
     );
   }
